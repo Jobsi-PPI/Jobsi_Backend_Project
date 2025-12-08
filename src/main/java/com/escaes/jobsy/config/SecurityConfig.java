adc329197af908af7c092ffc12b002831c77ec77
Juanchito1106 <jandrescorrea19@gmail.com>
2025-12-08 16:57:12 -0500

    Habilito el método de PATCH en el localhost 5731 y modifico lógica para cambiar Estado del Job

--- src/main/java/com/escaes/jobsy/config/SecurityConfig.java
+++ src/main/java/com/escaes/jobsy/config/SecurityConfig.java
@@ -86,7 +86,7 @@ public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Excepti
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(List.of("http://localhost:5173", "http://localhost:8080"));
-        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT","PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
 
